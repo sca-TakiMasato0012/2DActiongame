1a00793b546ec9c31585d1cb1fbd8a4658c646b5
sosei <sdt22cge0016@edu.sca.ac.jp>
2023-05-25 15:02:18 +0900

    コウモリの挙動を変更

--- Assets/sosei/Enemy/bat/Scripts/batController.cs
+++ Assets/sosei/Enemy/bat/Scripts/batController.cs
@@ -6,13 +6,14 @@ public class batController : MonoBehaviour
 {
     [SerializeField]
     private float BatSpeed;//コウモリの移動速度
+
     [SerializeField]
     private float Angle;//コウモリの角度
 
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
@@ -21,8 +22,13 @@ public class batController : MonoBehaviour
         // 左から右に移動
         transform.position += new Vector3(-BatSpeed, Angle, 0) * Time.deltaTime;
 
+        if(transform.position.y <= 1) 
+        {
+            Angle =BatSpeed *  0;
+        }
+       
         //画面上方に消えたらコウモリを消去
-       if (transform.position.x <= -10.0f)
+            if (transform.position.x <= -10.0f)
        {
            Destroy(gameObject);
        }
