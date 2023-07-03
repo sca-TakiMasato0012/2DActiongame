ff98edd5bffd603c64c1c7f8dad2053a62ebe509
Ito Shugo <21cge0002is@edu.sca.ac.jp>
2023-07-03 12:26:54 +0900

    カメラ直しました

---
+++ Assets/瀧/Scripts/GoburinController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoburinController : MonoBehaviour
+{
+    private MeshRenderer sr = null;
+
+    void Start()
+    {
+        sr = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        if(sr.isVisible)
+        {
+            Debug.Log("aioeagh");
+        }
+    }
+}
