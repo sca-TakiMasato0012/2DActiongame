4ac6fc89480af708486a3f4f9927dcbe8eee5adc
goto <taki>
2023-07-15 14:58:27 +0900

    ゴブリンにちょっと設定加えました

--- Assets/瀧/Scripts/GoburinController.cs
+++ Assets/瀧/Scripts/GoburinController.cs
@@ -1,24 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine.Unity;
+using Spine;
 
 public class GoburinController : MonoBehaviour
 {
     public SearchArea searchArea;
 
+    public GameObject target;//playerの取得
+
     private bool isSearchArea = false;
+
+    [SerializeField]
+    private string Start_Animation = "";
+    [SerializeField]
+    private string After_Animation = "";
+
+    private SkeletonAnimation skeletonAnimation = default;
+
+    private Spine.AnimationState spineAnimationState = default;
     void Start()
     {
-//        anim = GetComponent<Animator>();
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+
+        spineAnimationState = skeletonAnimation.AnimationState;
     }
+
+    bool isAnim = false;
 
     void Update()
     {
+        Vector2 player = target.transform.position;
+
         isSearchArea = searchArea.IsSearching();
 
-        if(isSearchArea)
+        if(isSearchArea&&!isAnim)
         {
+            PlayAnimation(Start_Animation);//アニメーションを再生
+            isAnim = true;
+
             Debug.Log("はいってるよ");
         }
     }
+
+    private void PlayAnimation(string name)
+    {
+
+        spineAnimationState.SetAnimation(0, name, true);
+    }
+
 }
