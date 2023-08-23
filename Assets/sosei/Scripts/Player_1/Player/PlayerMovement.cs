f1fe935aa413670e909d276a0b3ae6bf19add1a1
sosei <sdt22cge0016@edu.sca.ac.jp>
2023-08-23 12:08:35 +0900

    player編集

--- Assets/sosei/Scripts/Player_1/Player/PlayerMovement.cs
+++ Assets/sosei/Scripts/Player_1/Player/PlayerMovement.cs
@@ -22,6 +22,7 @@ public class PlayerMovement : MonoBehaviour {
 
 		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
+		
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			jump = true;
---
+++ Assets/sosei/Scripts/Player_pd/P_con.cs
@@ -0,0 +1,54 @@
+﻿using Spine;
+using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_con : MonoBehaviour
+{
+    [SerializeField]
+    private float P_moveSpeed= 0f;
+
+    [SerializeField]
+    private string taiki_ken_Animation = "";
+    [SerializeField]
+    private string taiki_yumi_Animation = "";
+    [SerializeField]
+    private string run_Animation = "";
+    [SerializeField]
+    private string dash_Animation = "";
+
+    private SkeletonAnimation skeletonAnimation = default;
+
+    private Spine.AnimationState spineAnimationState = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+
+        spineAnimationState = skeletonAnimation.AnimationState;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput) * P_moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+
+
+
+
+       
+
+    }
+    public void PlayAnimation(string name)
+    {
+
+        spineAnimationState.SetAnimation(0, taiki_ken_Animation, true);
+    }
+}
