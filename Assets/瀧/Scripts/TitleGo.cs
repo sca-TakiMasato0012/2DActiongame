244066bb5b8ce53ff2fc9ec61d284cbbfbd314be
Taki <21cge0012tm@edu.sca.ac.jp>
2023-09-08 17:14:39 +0900

    ステージ2に移行ととりあえず、ゴールフラッグとったらゲームクリアにしました。ゲームクリア画面も制作完了

---
+++ Assets/瀧/Scripts/TitleGo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleGo : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
+    }
+}
